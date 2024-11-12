336f93b58beda0dcabb2a199d31da9dfd5037189
Vladislav Pitsukha <pitsukhavladislav@gmail.com>
2024-11-12 23:25:56 +0100

    feat: add new components & funcs

---
+++ src/types/typeClotheCard.ts
@@ -0,0 +1,72 @@
+import { createClotheCards } from '@/utils/importImages';
+
+export type ClotheObjProps = {
+    id: number;
+    title: string;
+    price: number;
+    option?: number;
+}
+
+export type ClotheCard = ClotheObjProps & {
+    img: any;
+}
+
+export const TshirtsObjs: ClotheObjProps[] = [
+    {
+        id: 1,
+        title: "T-shirt with Tape Details",
+        price: 120,
+    },
+    {
+        id: 2,
+        title: "Sleeve Striped T-shirt",
+        price: 160,
+        option: 30,
+    },
+    {
+        id: 3,
+        title: "Courage Graphic T-shirt",
+        price: 145,
+    }
+]
+
+export const ShirtObjs: ClotheObjProps[] = [
+    {
+        id: 1,
+        title: "Checkered Shirt",
+        price: 180,
+    },
+    {
+        id: 2,
+        title: "Vertical Striped Shirt",
+        price: 232,
+        option: 30,
+    }
+]
+
+export const JeansObjs: ClotheObjProps[] = [
+    {
+        id: 1,
+        title: "Skinny Fit Jeans",
+        price: 260,
+        option: 20,
+    },
+    {
+        id: 2,
+        title: "Faded Skinny Jeans",
+        price: 210,
+    }
+]
+
+export const ShortsObj: ClotheObjProps[] = [
+    {
+        id: 1,
+        title: "Loose Fit Bermuda Shorts",
+        price: 80,
+    }
+]
+
+export const clotheTShirtsCards = createClotheCards('t-shirt', TshirtsObjs);
+export const clotheShirtCards = createClotheCards('shirt', ShirtObjs);
+export const clotheShortsCards = createClotheCards('short', ShortsObj);
+export const clotheJeansCards = createClotheCards('jeans', JeansObjs);
