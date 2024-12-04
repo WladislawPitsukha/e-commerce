289f124d0721c238220bef5a6ac82d6a390f664a
Vladislav Pitsukha <pitsukhavladislav@gmail.com>
2024-12-04 18:19:44 +0100

    feat: add new changes & new component of pazzle

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'integral': ['"Integral CF"', 'sans-serif'],
+        'inter': ['Inter', 'sans-serif'],
+        'satoshi': ['Satoshi', 'sans-serif'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundColor: {
+        'color-white': 'rgba(255, 255, 255, 1)',
+        'light-gray': 'rgba(240, 240, 240, 1)',
+      }
     },
   },
   plugins: [],
