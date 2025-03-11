85754f9fb53aadd99dcb14629afde5cb4aa8ff04
Vladislav Pitsukha <pitsukhavladislav@gmail.com>
2025-03-11 20:49:35 +0100

    feat: add new func & component

---
+++ src/utils/randomCards.ts
@@ -0,0 +1,32 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import React from "react";
+
+import { ClotheObjProps } from "@/types/typeClotheCard";
+
+import { arrShirtsObjs } from "@/constants/cardsMainObj/arrShirtsObjs";
+import { arrJeansObjs } from "@/constants/cardsMainObj/arrJeansObjs";
+import { arrTshirtsObjs } from "@/constants/cardsMainObj/arrTshirtsObjs";
+import { arrShortsObj } from "@/constants/cardsMainObj/arrShortsObjs";
+
+export default function useRandomCards(sectionType: "new" | "top") {
+    const [randomCards, setRandomCards] = useState<ClotheObjProps[]>([]);
+    const allCards: ClotheObjProps[] = [arrShirtsObjs, arrJeansObjs, arrShortsObj, arrTshirtsObjs].flat(2);
+
+    const getRandomCard = () => {
+        const sortedArray = allCards.sort(() => 0.5 - Math.random());
+        const slicedArray = sortedArray.slice(0, 4);
+
+        setRandomCards(slicedArray);
+    }
+
+    useEffect(() => {
+        getRandomCard()
+    }, []);
+
+    return {
+        randomCards,
+        getRandomCard
+    }
+}
