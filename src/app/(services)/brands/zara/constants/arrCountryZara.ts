da23de6375ef98ad586ecd7a1eb67cba9f6c8c8b
Vladislav Pitsukha <pitsukhavladislav@gmail.com>
2025-03-25 23:57:40 +0100

    feat: add new types & arrays

---
+++ src/app/(services)/brands/zara/constants/arrCountryZara.ts
@@ -0,0 +1,154 @@
+import { CountryDataProps } from "../../types/typeCountryData";
+
+export const arrCountryData: CountryDataProps[] = [
+    {
+        id: 1,
+        country: "Spain",
+        financeData: {
+            revenue: 3.1,
+            marketShare: 9.2,
+            storeCount: 290,
+            onlinePresence: 88
+        }
+    },
+    {
+        id: 2,
+        country: "France",
+        financeData: {
+            revenue: 3.4,
+            marketShare: 10.0,
+            storeCount: 320,
+            onlinePresence: 90
+        }
+    },
+    {
+        id: 3,
+        country: "Italy",
+        financeData: {
+            revenue: 2.8,
+            marketShare: 8.5,
+            storeCount: 280,
+            onlinePresence: 85
+        }
+    },
+    {
+        id: 4,
+        country: "United Kingdom",
+        financeData: {
+            revenue: 3.2,
+            marketShare: 9.8,
+            storeCount: 310,
+            onlinePresence: 92
+        }
+    },
+    {
+        id: 5,
+        country: "Germany",
+        financeData: {
+            revenue: 3.5,
+            marketShare: 10.2,
+            storeCount: 340,
+            onlinePresence: 88
+        }
+    },
+    {
+        id: 6,
+        country: "USA",
+        financeData: {
+            revenue: 4.8,
+            marketShare: 12.5,
+            storeCount: 420,
+            onlinePresence: 95
+        }
+    },
+    {
+        id: 7,
+        country: "China",
+        financeData: {
+            revenue: 5.2,
+            marketShare: 15.8,
+            storeCount: 580,
+            onlinePresence: 90
+        }
+    },
+    {
+        id: 8,
+        country: "Japan",
+        financeData: {
+            revenue: 2.9,
+            marketShare: 8.8,
+            storeCount: 250,
+            onlinePresence: 82
+        }
+    },
+    {
+        id: 9,
+        country: "South Korea",
+        financeData: {
+            revenue: 2.4,
+            marketShare: 7.2,
+            storeCount: 180,
+            onlinePresence: 88
+        }
+    },
+    {
+        id: 10,
+        country: "Russia",
+        financeData: {
+            revenue: 2.1,
+            marketShare: 6.5,
+            storeCount: 160,
+            onlinePresence: 75
+        }
+    },
+    {
+        id: 11,
+        country: "India",
+        financeData: {
+            revenue: 1.8,
+            marketShare: 5.4,
+            storeCount: 140,
+            onlinePresence: 70
+        }
+    },
+    {
+        id: 12,
+        country: "Brazil",
+        financeData: {
+            revenue: 1.5,
+            marketShare: 4.8,
+            storeCount: 120,
+            onlinePresence: 65
+        }
+    },
+    {
+        id: 13,
+        country: "Mexico",
+        financeData: {
+            revenue: 1.2,
+            marketShare: 3.8,
+            storeCount: 95,
+            onlinePresence: 60
+        }
+    },
+    {
+        id: 14,
+        country: "Canada",
+        financeData: {
+            revenue: 1.9,
+            marketShare: 5.8,
+            storeCount: 150,
+            onlinePresence: 85
+        }
+    },
+    {
+        id: 15,
+        country: "Australia",
+        financeData: {
+            revenue: 1.6,
+            marketShare: 4.9,
+            storeCount: 130,
+            onlinePresence: 80
+        }
+    }
+];
