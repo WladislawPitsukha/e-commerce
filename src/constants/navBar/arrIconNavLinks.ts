e55fe50e036275b9093e849add1e5b3a5e09735c
Vladislav Pitsukha <pitsukhavladislav@gmail.com>
2025-03-29 08:40:35 +0100

    feat: add new files

---
+++ src/constants/navBar/arrIconNavLinks.ts
@@ -0,0 +1,14 @@
+import { NavLinkProps } from "@/types/typeNavLinks";
+import { RiAccountCircleLine } from "react-icons/ri";
+import { SlBasket } from "react-icons/sl";
+
+export const arrIconNavlinks: NavLinkProps[] = [
+    {
+        icon: SlBasket,
+        link: "",
+    },
+    {
+        icon: RiAccountCircleLine,
+        link: ""
+    }
+];
