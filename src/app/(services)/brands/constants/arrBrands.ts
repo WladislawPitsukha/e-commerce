144e2e4193806113631d4e1181aa4a139609e534
Vladislav Pitsukha <pitsukhavladislav@gmail.com>
2025-04-03 22:44:52 +0200

    feat: add new components for project

---
+++ src/app/(services)/brands/constants/arrBrands.ts
@@ -0,0 +1,44 @@
+import { BrandsProps } from "../types/typeBrands";
+
+export const arrBrands: BrandsProps[] = [
+    {
+        id: 1,
+        name: "Zara",
+        links: {
+            mainLink: "/brands/zara",
+            webpageLink: "https://www.zara.com"
+        }
+    },
+    {
+        id: 2,
+        name: "Calvin Klein",
+        links: {
+            mainLink: "/brands/calvinklein",
+            webpageLink: "https://www.calvinklein.com"
+        }
+    },
+    {
+        id: 3,
+        name: "Gucci",
+        links: {
+            mainLink: "/brands/gucci", 
+            webpageLink: "https://www.gucci.com"
+        }
+    },
+    {
+        id: 4,
+        name: "Prada",
+        links: {
+            mainLink: "/brands/prada",
+            webpageLink: "https://www.prada.com"
+        }
+    },
+    {
+        id: 5,
+        name: "Versace",
+        links: {
+            mainLink: "/brands/versace",
+            webpageLink: "https://www.versace.com"
+        }
+    }
+];
