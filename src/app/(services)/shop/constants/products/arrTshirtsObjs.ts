609084f7cec1329621252637ac37c7f4d91cfb5b
Vladislav Pitsukha <pitsukhavladislav@gmail.com>
2025-04-24 09:39:02 +0200

    refactor: change th location for this files

--- src/app/(services)/shop/constants/products/arrTshirtsObjs.ts
+++ src/app/(services)/shop/constants/products/arrTshirtsObjs.ts
@@ -2,31 +2,37 @@ import TShirt1 from "@/assets/img/t-shirt/t-shirt1.png";
 import TShirt2 from "@/assets/img/t-shirt/t-shirt2.png";
 import TShirt3 from "@/assets/img/t-shirt/t-shirt3.png";
 
-import { ClotheObjProps } from "@/types/typeClotheCard";
+import { ClotheMainObjProps } from "@/types/typeProductCard";
 
-export const arrTshirtsObjs: ClotheObjProps[] = [
+export const arrTshirtsObjs: ClotheMainObjProps[] = [
     {
         id: 1,
         img: TShirt1,
         title: "T-shirt with Tape Details",
-        price: 120,
-        option: false,
-        procent: 0
+        price: {
+            mainPrice: 120,
+            option: false,
+            procent: 0,
+        },
     },
     {
         id: 2,
         img: TShirt2,
         title: "Sleeve Striped T-shirt",
-        price: 160,
-        option: true,
-        procent: 30
+        price: {
+            mainPrice: 160,
+            option: true,
+            procent: 30,
+        },
     },
     {
         id: 3,
         img: TShirt3,
         title: "Courage Graphic T-shirt",
-        price: 145,
-        option: false,
-        procent: 0
+        price: {
+            mainPrice: 145,
+            option: false,
+            procent: 0,
+        },
     }
 ];
