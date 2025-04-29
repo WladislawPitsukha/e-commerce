2bd70b5986d806246bd86c3c340d58fa96ec4bbf
Vladislav Pitsukha <pitsukhavladislav@gmail.com>
2025-04-29 20:35:36 +0200

    feat: add new funcs

---
+++ src/app/(services)/shop/utils/products.ts
@@ -0,0 +1,38 @@
+import { ProductCardProps } from "@/types/typeProductCard";
+import { casualwearProducts } from "../[category]/casual/arrDataProducts";
+import { officewearProducts } from "../[category]/office/arrDataProducts";
+import { partywearProducts } from "../[category]/party/arrDataProducts";
+import { summerwearProducts } from "../[category]/summer/arrDataProducts";
+import { winterwearProducts } from "../[category]/winter/arrDataProducts";
+import { activewearProducts } from "../[category]/active/arrDataProducts";
+
+//TODO: finish this file to display the products of the selected category
+
+
+export const allProducts: ProductCardProps[] = [
+    ...activewearProducts,
+    ...casualwearProducts,
+    ...officewearProducts,
+    ...partywearProducts,
+    ...summerwearProducts,
+    ...winterwearProducts
+];
+
+export const getProductsByCategory = (category: string): ProductCardProps[] => {
+    switch (category.toLowerCase()) {
+        case 'activewear':
+        return activewearProducts;
+        case 'casualwear':
+        return casualwearProducts;
+        case 'officewear':
+        return officewearProducts;
+        case 'partywear':
+        return partywearProducts;
+        case 'summerwear':
+        return summerwearProducts;
+        case 'winterwear':
+        return winterwearProducts;
+        default:
+        return allProducts;
+    }
+};
