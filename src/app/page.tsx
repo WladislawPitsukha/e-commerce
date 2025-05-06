28044596d74a1b8e9ed6a25fffb5d0bbe8981dde
Vladislav Pitsukha <pitsukhavladislav@gmail.com>
2025-05-06 19:44:07 +0200

    feat: add a little change for structure main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,54 @@
-import Footer from "@/components/footer";
-import MainPage from "@/components/mainPage";
-import NavBar from "@/components/navbar";
+"use client"
 
-export default function Home() {
-  return (
-    <div className="flex flex-col justify-center bg-white">
-      <NavBar />
-      <MainPage />
-      <Footer />
-    </div>
-  );
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import NavBar from "@/components/navbar";
+import SectionCards from "@/components/sectionCards";
+import SectionComment from "@/components/sectionComment";
+import SectionStyle from "@/components/sectionStyle";
+import { arrDressStyle } from "@/constants/arrDressStyle";
+import useRandomCards from "@/utils/randomCards";
+import { useEffect } from "react";
+
+
+
+export default function MainPage() {
+    const newArrivals = useRandomCards('new');
+    const topSelling = useRandomCards('top');
+
+    useEffect(() => {
+        newArrivals.getRandomCard();
+        topSelling.getRandomCard();
+    }, []);
+
+    return(
+        <div className="flex flex-col items-center bg-white">
+            <Header />
+            {[
+                {
+                    title: "New Arrays",
+                    array: newArrivals.randomCards,
+                    link: "",
+                },
+                {
+                    title: 'Top selling',
+                    array: topSelling.randomCards,
+                    link: "",
+                }
+            ].map((item, index) =>(
+                <SectionCards 
+                    key={index}
+                    {...item}
+                />
+            ))}
+            <SectionStyle 
+                arr={arrDressStyle}
+                num1={0}
+                num2={2}
+                num3={2}
+                num4={4}
+            />
+            <SectionComment />
+        </div>
+    )
 }
